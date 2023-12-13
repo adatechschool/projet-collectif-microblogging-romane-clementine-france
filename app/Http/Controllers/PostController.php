da4cd4a1c0e162b71759577550d91df142ba0d63
clementine <cleriv.72@gmail.com>
2023-12-13 15:21:46 +0100

    ajout de commentaires suite code review

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Support\Facades\Log;
 // on fait référence au fichier Post dans Model que l'on appelera "Posts"
 
 class PostController extends Controller
@@ -14,7 +13,6 @@ class PostController extends Controller
     {
         //On demande à laravel de récupérer tous les objets dans post
         $posts = Post::orderBy('created_at', 'desc')->with('User')->get();
-
         // dd($posts);
         //On retourne la vue avec les posts intégrés
         // la methode compact permet de transformer les données en tableau pour les rendre accessibles à la vue.
@@ -40,12 +38,16 @@ class PostController extends Controller
     //la fonction store est actionnée grâce à la méthod post du formulaire généré par create et permet de récupérer et d'envoyer les données vers la BDD.
     public function store(Request $request)
     {
+        //Cette ligne récupère le résultat du formulaire dans create.blade.php sur l'input nommé image. C'est un input spécifique qui récupère les fichiers.
         $image = $request->file('image');
-        $filename = now()->timestamp . '.' . $image->getClientOriginalExtension();
 
+        //Cette ligne permet de définir un nouveau nom au fichier qui est récupéré par l'input image. Le nom correspond au nombre de sec depuis 1968 + l'extension de fichier du client (png/jpeg etc...)
+        $filename = now()->timestamp . '.' . $image->getClientOriginalExtension();
+        
+        //Cette ligne permet de déplacer le fichier image dans le dossier public images/storage et de lui appliquer son nouveau nom.
         $image->move(public_path('images/storage'), $filename);
 
-        $post = Post::create([
+        Post::create([
             'user_id' => $request->input('user_id'),
             'content' => $request->input('content'),
             'image' => 'images/storage/'.$filename,
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -16,13 +16,20 @@
                 {{ session('success') }}
             </div>
         @endif
+
+        {{-- On fait une boucle pour afficher les posts --}}
         @foreach ($posts as $post)
             <div class="card" style='width:60%; margin-bottom:2rem'>
                 <h5 class="card-title">Published by
+                    {{-- Cette ligne permet de faire apparaître le nom de l'auteur du post, désigné par "$post->user->name" et de rediriger vers la route profile.post qui correspond à l'id de l'auteur. --}}
                     <a href="{{ route('profile.post', [$post->user_id]) }}"> {{ Str::upper($post->user->name) }} </a>at
                     {{ $post->created_at }}
                 </h5>
-                <img class="card-img-top" style='height:30rem; object-fit:contain; width:auto; background-color:black'
+                {{-- L'image est affichée grâce au paramètre passé dans la src "($post->image)" celui-ci contient le chemin relatif vers l'image qui est téléchargé au moment de la génération du post. --}}
+                <img class="card-img-top" 
+                    style='height:30rem; 
+                    object-fit:contain; 
+                    width:auto; background-color:black'
                     src="{{ asset($post->image) }}" alt="Card image cap">
                 <div class="card-body">
                     <p class="card-text">{{ $post->content }}</p>
